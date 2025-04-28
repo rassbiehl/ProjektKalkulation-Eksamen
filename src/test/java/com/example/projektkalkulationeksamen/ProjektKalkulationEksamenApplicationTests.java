0d427684eea088261f671d699bd2101f910b4be1
VictorIrmand <vini0003@stud.kea.dk>
2025-04-28 12:51:10 +0200

    first commit

---
+++ src/test/java/com/example/projektkalkulationeksamen/ProjektKalkulationEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektkalkulationeksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKalkulationEksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
