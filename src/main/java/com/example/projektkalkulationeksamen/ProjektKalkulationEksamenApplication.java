a461c1212d770c4ba9cd9d5ac58eee4f61fc28bc
rasbiehl <rasbiehl@gmail.com>
2025-05-23 20:27:46 +0200

    det er nu fixed, så hvis man opdaterer en bruger, så behøver man ikke at taste noget ind, hvis man ikke ønsker at skrive sit username.

--- src/main/java/com/example/projektkalkulationeksamen/ProjektKalkulationEksamenApplication.java
+++ src/main/java/com/example/projektkalkulationeksamen/ProjektKalkulationEksamenApplication.java
@@ -2,12 +2,15 @@ package com.example.projektkalkulationeksamen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjektKalkulationEksamenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjektKalkulationEksamenApplication.class, args);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("123"));
     }
 
 }
